c18f70feaabe6fc9b986ffd0bc8e839d56c26dde
gimzaezin <gimzaezin@gmail.com>
2025-02-10 15:39:42 +0900

    init commit

---
+++ src/test/java/com/gimzazin/tiketflow/TiketFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimzazin.tiketflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
